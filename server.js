4472d7926f3196b2dd8cf5966cb6c567e5ded72f
AlexandreAbreuCzr <alexandreabreuczr@gmail.com>
2025-06-20 14:11:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server está rodando');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server rodando na porta ${PORT}`);
 });
