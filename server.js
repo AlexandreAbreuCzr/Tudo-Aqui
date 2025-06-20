4e84fd0188706484fe95de82ea10d3716a8cf676
AlexandreAbreuCzr <alexandreabreuczr@gmail.com>
2025-06-20 15:38:58 -0300

    aserver cors

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
-
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
+// Middleware para liberar CORS (liberar acesso para qualquer origem)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') // permite qualquer origem acessar
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS') // métodos permitidos
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization') // headers permitidos
+  next()
+})
+
 server.use(middlewares)
 server.use('/api', router)
+
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
